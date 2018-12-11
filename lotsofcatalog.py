793011b7c36afbdd40bcf7b97860a150dea2ae1b
Ahmed Almaged <ahmed01799@gmail.com>
2018-12-11 18:42:57 +0300

    Initial commit

---
+++ lotsofcatalog.py
@@ -0,0 +1,133 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import Category, Base, Item, User
+
+engine = create_engine('sqlite:///Category.db')
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
+
+
+# Create dummy user
+User1 = User(name="Robo Barista", email="tinnyTim@udacity.com",
+             picture='https://pbs.twimg.com/profile_images/2671170543/18debd694829ed78203a5a36dd364160_400x400.png')
+session.add(User1)
+session.commit()
+
+# Menu for UrbanBurger
+category1 = Category(user_id=1, name="Soccer")
+
+session.add(category1)
+session.commit()
+
+item1 = Item(user_id=1, name="Veggie", description="Juicy grilled veggie patty with tomato mayo and lettuce"
+                , category=category1)
+
+session.add(item1)
+session.commit()
+
+
+item2 = Item(user_id=1, name="French Fries", description="with garlic and parmesan",
+                        category=category1)
+
+session.add(item2)
+session.commit()
+
+menuItem2 = Item(user_id=1, name="Chicken Burger", description="Juicy grilled chicken patty with tomato mayo and lettuce",
+                        category=category1)
+
+
+session.add(menuItem2)
+session.commit()
+# Menu for UrbanBurger
+category2 = Category(user_id=1, name="Football")
+
+session.add(category2)
+session.commit()
+
+item1 = Item(user_id=1, name="Veggie", description="Juicy grilled veggie patty with tomato mayo and lettuce"
+                , category=category2)
+
+session.add(item1)
+session.commit()
+
+
+item2 = Item(user_id=1, name="French Fries", description="with garlic and parmesan",
+                        category=category2)
+
+session.add(item2)
+session.commit()
+
+menuItem2 = Item(user_id=1, name="Chicken Burger", description="Juicy grilled chicken patty with tomato mayo and lettuce",
+                        category=category2)
+
+session.add(menuItem2)
+session.commit()
+
+# Create dummy user
+User2 = User(name="Ahmed Abdullah", email="ahmed@udacity.com",
+             picture='https://pbs.twimg.com/profile_images/2671170543/18debd694829ed78203a5a36dd364160_400x400.png')
+session.add(User2)
+session.commit()
+
+# Menu for UrbanBurger
+category1 = Category(user_id=2, name="Basketball")
+
+session.add(category1)
+session.commit()
+
+item1 = Item(user_id=2, name="Veggie", description="Juicy grilled veggie patty with tomato mayo and lettuce"
+                , category=category1)
+
+session.add(item1)
+session.commit()
+
+
+item2 = Item(user_id=2, name="French Fries", description="with garlic and parmesan",
+                        category=category1)
+
+session.add(item2)
+session.commit()
+
+menuItem2 = Item(user_id=2, name="Chicken Burger", description="Juicy grilled chicken patty with tomato mayo and lettuce",
+                        category=category1)
+
+
+session.add(menuItem2)
+session.commit()
+# Menu for UrbanBurger
+category2 = Category(user_id=2, name="Hockey")
+
+session.add(category2)
+session.commit()
+
+item1 = Item(user_id=2, name="Veggie", description="Juicy grilled veggie patty with tomato mayo and lettuce"
+                , category=category2)
+
+session.add(item1)
+session.commit()
+
+
+item2 = Item(user_id=2, name="French Fries", description="with garlic and parmesan",
+                        category=category2)
+
+session.add(item2)
+session.commit()
+
+menuItem2 = Item(user_id=2, name="Chicken Burger", description="Juicy grilled chicken patty with tomato mayo and lettuce",
+                        category=category2)
+
+session.add(menuItem2)
+session.commit()
+print "added menu items!"
